819339b63dbc22175d46d4831727dd8d318e3071
Bazán Romina <danielabazan94@gmail.com>
2019-03-10 16:32:31 -0300

    calcularCosto() corregido

--- Viajes/Paquete.vb
+++ Viajes/Paquete.vb
@@ -1,30 +1,40 @@
 ﻿Public Class Paquete
-    Implements IAbono
+    Implements Abono
     Private _fechaFinal As Date
     Private _fechaInicial As Date
-    'Private _numeroDias As Integer
-    Private _numeroMiembros As Integer
     Private _cliente As Cliente
-    Private _listaPasajeros As List(Of Pasajero)
-    Private _listaServicios As List(Of Servicio)
-    Private ReadOnly Property FechaFinal As Date
+    Private _pasajeros As List(Of Pasajero)
+    Private _servicios As List(Of Servicio)
+    Public Function CalcularCosto() As Single Implements Abono.CalcularCosto
+        ''SUMATORIA DE LOS COSTOS DE LOS SERVICIOS  
+        Dim total As Single
+        For Each servicio In _servicios.OfType(Of Hotel)
+            total += servicio.CalcularCosto
+        Next
+        For Each servicio In _servicios.OfType(Of Pasaje)
+            total += servicio.CalcularCosto
+        Next
+
+        Return total
+    End Function
+    Public ReadOnly Property FechaFinal As Date
         Get
             Return _fechaFinal
         End Get
     End Property
-    Private ReadOnly Property FechaInicial As Date
+    Public ReadOnly Property FechaInicial As Date
         Get
             Return _fechaInicial
         End Get
     End Property
-    Private ReadOnly Property NumeroDias As Integer
+    Public ReadOnly Property NumeroDias As Integer
         Get
-            Return calcularNumeroDias()
+            Return DateDiff(DateInterval.Day, FechaInicial, FechaFinal)
         End Get
     End Property
-    Private ReadOnly Property NumeroMiembros As Integer
+    Public ReadOnly Property NumeroMiembros As Integer
         Get
-            Return CalcularNumeroMiembros()
+            Return _pasajeros.Count()
         End Get
     End Property
     Public Property Cliente As Cliente
@@ -35,51 +45,36 @@
             _cliente = value
         End Set
     End Property
-    Public Sub addPasajero(var As Pasajero)
-        _listaPasajeros.Add(var)
+    ''====> CARDINALIDAD CON PASAJERO 
+    Public Sub AddPasajero(variable As Pasajero)
+        _pasajeros.Add(variable)
     End Sub
-    'Public Sub removePasajero(var As Pasajero)
-    '    _listaPasajeros.Remove(var)
-    'End Sub
-    Public Function getAllPasajeros() As List(Of Pasajero)
-        Return _listaPasajeros
-    End Function
-    Public Sub addServicio(var As Servicio)
-        _listaServicios.Add(var)
+    Public Sub RemovePasajero(variable As Pasajero)
+        _pasajeros.Remove(variable)
     End Sub
-    'Public Sub removeServicio(var As Servicio)
-    '    _listaServicios.Remove(var)
-    'End Sub
-    Public Function getAllServicio() As List(Of Servicio)
-        Return _listaServicios
+    Public Function GetAllPasajeros() As List(Of Pasajero)
+        Return _pasajeros
     End Function
-    Public Function calcularNumeroDias() As Integer
-        Return FechaFinal.Day - FechaInicial.Day
-    End Function
-    Public Function CalcularNumeroMiembros() As Integer
-        _numeroMiembros = _listaPasajeros.Count()
-        Return _numeroMiembros
-    End Function
-    Public Function CalcularCosto() As Single Implements IAbono.CalcularCosto
-        Dim total As Single
-        '==> Falta sacar los importes de cada servicio y sumarlos.!!
-        'For Each servicio In _listaServicios
-        '    total += servicio.calcularCosto()
-        'Next
-        Return total
+    ''=====> CARDINALIDAD 1-M CON SERVICIOS CLASE RESPONSABLE
+    Public Sub AddServicio(variable As Servicio)
+        _servicios.Add(variable)
+    End Sub
+    Public Sub RemoveServicio(variable As Servicio)
+        _servicios.Remove(variable)
+    End Sub
+    Public Function GetAllServicios() As List(Of Servicio)
+        Return _servicios
     End Function
     Public Overrides Function ToString() As String
-        Dim var As String = ""
-        For Each elemento In _listaServicios
-            var += " - "
-            var += elemento.ToString()
+        Dim cadena As String = ""
+        ''concatenacion de los toString() de los  servicios
+        For Each servicio In _servicios
+            cadena += servicio.ToString() & " - "
         Next
-
-        Return var
+        Return cadena
     End Function
-
     Sub New()
-        _listaPasajeros = New List(Of Pasajero)
-        _listaServicios = New List(Of Servicio)
+        _pasajeros = New List(Of Pasajero)
+        _servicios = New List(Of Servicio)
     End Sub
 End Class
